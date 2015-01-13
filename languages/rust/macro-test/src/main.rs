2d6a49a4f1edc076df7bc0b1f91156c92f8dd92a
psyomn <lethaljellybean@gmail.com>
2015-01-12 23:28:39 -0500

    rust-macros: add another simple example

--- languages/rust/macro-test/src/main.rs
+++ languages/rust/macro-test/src/main.rs
@@ -1,8 +1,14 @@
-#![feature(macro_rules)]
+/* rustc 1.0.0-dev (2e2372c6c 2015-01-09 09:50:06 +0000) */
 
 macro_rules! myassign {
     ($inp:ident -> (($e:expr))) => (
         $inp = $e;
+        );
+}
+
+macro_rules! myassign2 {
+    ($inp:ident) => (
+        $inp += 5;
         );
 }
 
@@ -16,6 +22,14 @@ fn benc_without_macro() {
 
 fn main() {
     let mut a : int = 0;
+    let mut x : int = 0;
+
     myassign!(a -> ((2 + 2)));
+
+    myassign2!(x); myassign2!(x);
+    myassign2!(x); myassign2!(x);
+
     println!("{}", a);
+    println!("{}", x);
 }
+
