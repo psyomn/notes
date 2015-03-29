d040ae96ed9e888d343bc4baa870b90e3c746756
psyomn <lethaljellybean@gmail.com>
2015-03-29 17:47:09 -0400

    minibattle: add some more info for entity

--- languages/haskell/minibattle/src/Entity.hs
+++ languages/haskell/minibattle/src/Entity.hs
@@ -16,19 +16,24 @@ data GameEntity = GameEntity {
    , maxMP      :: Integer
    , hitpoints  :: Integer
    , maxHP      :: Integer
-   , Experience :: Integer
-   , NextLevel  :: Integer
+   , experience :: Integer
+   , nextLevel  :: Integer
+   , level      :: Integer
    }
 
 -- Prettier printing for the custom type
 instance Show GameEntity where
   show (GameEntity {name=n, strength=s, defense=d, speed=spe, magic=m,
-        maxMP=mmp, hitpoints=hp, maxHP=mhp}) = n ++ "\n"
-          ++ " Strength     : " ++ show s   ++ "\n"
-          ++ " Defense      : " ++ show d   ++ "\n"
-          ++ " Speed        : " ++ show spe ++ "\n"
-          ++ " Magic        : " ++ show m   ++ "/"  ++ show mmp ++ "\n"
-          ++ " Hitpoints    : " ++ show hp  ++ "/"  ++ show mhp ++ "\n"
+                    maxMP=mmp, hitpoints=hp, maxHP=mhp, level=l,
+                    nextLevel=nl, experience=xp}) =
+    n ++ " | Level: "     ++ show l   ++ "\n"
+    ++ " Strength     : " ++ show s   ++ "\n"
+    ++ " Defense      : " ++ show d   ++ "\n"
+    ++ " Speed        : " ++ show spe ++ "\n"
+    ++ " Magic        : " ++ show m   ++ "/"  ++ show mmp ++ "\n"
+    ++ " Hitpoints    : " ++ show hp  ++ "/"  ++ show mhp ++ "\n"
+    ++ " Experience   : " ++ show xp  ++ "\n"
+    ++ " NextLevel    : " ++ show nl
 
 -- Get the name of the entity
 nameOf :: GameEntity -> String
@@ -44,7 +49,10 @@ makeDefault = GameEntity {
   , magic = 5
   , maxMP = 5
   , hitpoints = 10
-  , maxHP = 10 }
+  , maxHP = 10
+  , experience = 0
+  , nextLevel = 30
+  , level = 1 }
 
 -- Base attack would be str + any equipment (no eq impl atm)
 baseAttack :: GameEntity -> Integer
