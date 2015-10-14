e8a2759c61ec8042c56f0db0a8b6d4f67c4b9d58
psyomn <lethaljellybean@gmail.com>
2015-10-14 19:44:23 -0400

    rust/tomfoolery: can edit toml tables now

--- languages/rust/tomfoolery/src/main.rs
+++ languages/rust/tomfoolery/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     let fs: Vec<fn() -> ()> = vec![
         access_table_as_btreemap,
         print_toml_table_as_string,
+        mutate_tom,
     ];
 
     for f in fs {
@@ -68,5 +69,40 @@ fn print_toml_table_as_string() -> () {
     let toml_str = toml::encode(&results);
 
     println!("{}", toml_str);
+}
+
+
+fn mutate_tom() -> () {
+    println!("Mutating TOM!");
+
+    let mut f: File = match File::open("data.toml") {
+        Ok(v) => v,
+        Err(e) => panic!(e),
+    };
+
+    let mut s: String = String::new();
+    f.read_to_string(&mut s);
+
+    let mut parser = toml::Parser::new(&s);
+    let mut table = parser.parse().unwrap();
+
+    match table.get_mut(&"myconfig1".to_string()) {
+        Some(v) => {
+            match v {
+                &mut toml::Value::Table(ref mut inner) => {
+                    inner.insert("name".to_string(), toml::Value::String("LEL".to_string()));
+                    println!("inner table is: {:?}", inner);
+                },
+                _ => {},
+
+            }
+        },
+        None => {
+            println!("nothin to see here!");
+        }
+    }
+
+    println!("Finally printing the mutated toml");
+    println!("{}", toml::encode(&table));
 }
 
