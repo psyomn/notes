f3819cb7effbfded09b19cc71167e9beb63f4a37
psyomn <lethaljellybean@gmail.com>
2015-11-28 19:47:49 -0500

    languages/cxx: minor test in threading in cxx

---
+++ languages/cxx/threading/main.cxx
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <random>
+#include <cmath>
+
+using std::cout;
+using std::endl;
+using std::thread;
+
+void lamda_threads();
+void func_threads();
+
+int main(void) {
+  lamda_threads();
+  func_threads();
+
+  return 0;
+}
+
+void work_fn() {
+  std::random_device rng;
+  std::default_random_engine e1(rng());
+  std::uniform_int_distribution<int> uniform_dist(1, 10);
+  int rn = uniform_dist(e1);
+
+  for (int i = 0; i < 100000 * rn; ++i)
+  for (int j = 0; j < 10000; ++j);
+  cout << "Finished running" << endl;
+}
+
+void func_threads() {
+  int num = 4;
+  std::vector<thread> threads;
+
+  while(num--) {
+    threads.push_back(std::thread(work_fn));
+  }
+
+  for (auto& t_el : threads) {
+    t_el.join();
+  }
+}
+
+void lamda_threads() {
+  thread t([](){ cout << "some people call me a thread" << endl; }),
+         t2([](){ cout << "I'm some other thread as well!" << endl; }),
+         t3([](){ cout << "I am a potato" << endl; });
+  t.join();
+  t2.join();
+  t3.join();
+}
+
+/* */
