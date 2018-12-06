de696ea073963ead35a3c2b1c71e5f589690c7cf
Simon Symeonidis <lethaljellybean@gmail.com>
2018-12-06 16:55:02 -0500

    Get rid of config.sh

--- configh.sh
+++
@@ -1,7 +0,0 @@
-
-mkdir -p build-tools/
-
-if [ ! -f build-tools/plantuml.jar ]; then 
-  wget -O build-tools/plantuml.jar http://sourceforge.net/projects/plantuml/files/plantuml.jar/download
-fi 
-
