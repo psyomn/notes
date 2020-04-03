cc5bd80d16494c2e465fece35448d71c3b0d4944
Simon Symeonidis <lethaljellybean@gmail.com>
2020-04-03 01:37:31 -0400

    Add tcp client along with getopts

--- languages/c/little-shop-of-horrors/src/sendfile-horror.c
+++ languages/c/little-shop-of-horrors/src/sendfile-horror.c
@@ -7,25 +7,92 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 
+#include <linux/limits.h>
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <getopt.h>
 
+const int PORT = 6666;
 void server(void);
+void client(char*);
 
-int main(void) {
+int main(int argc, char *argv[]) {
+  int opt = 0;
+  char *path = NULL;
+  bool found_path = false;
+  bool found_client = false;
+  bool found_server = false;
+
+  while ((opt = getopt(argc, argv, "scf:")) != -1) {
+    switch (opt) {
+    case 's':
+      found_server = true;
+      break;
+    case 'c':
+      found_client = true;
+      break;
+    case 'f':
+      found_path = true;
+      path = optarg;
+      break;
+    default:
+      goto error;
+    }
+  }
+
+  if (found_server)
+    server();
+  else if (found_client && found_path)
+    client(path);
+  else
+    goto error;
 
   return 0;
+
+error:
+  printf("usage: \n");
+  printf("  %s (-s|-c|-f)", argv[0]);
+  exit(EXIT_FAILURE);
+  return -1;
+}
+
+void client(char *path) {
+  (void) path;
+
+  int client_fd = 0;
+  struct sockaddr_in server = {
+    .sin_family = AF_INET,
+    .sin_addr.s_addr = htonl(INADDR_LOOPBACK),
+    .sin_port = htons(PORT)
+  };
+
+  client_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (client_fd == -1) {
+    perror("client could not create socket");
+    exit(EXIT_FAILURE);
+  }
+
+  if (connect(client_fd, (struct sockaddr *)&server, sizeof(server)) == -1) {
+    perror("could not connect");
+    exit(EXIT_FAILURE);
+  }
+
+  const char *message = "hello there stalker";
+  send(client_fd, message, strlen(message), 0);
+  close(client_fd);
 }
 
 void server(void) {
-int server_fd;
+  int server_fd = 0;
   struct sockaddr_in address = {
     .sin_family = AF_INET,
     .sin_addr.s_addr = htonl(INADDR_LOOPBACK), // im not completely crazy ;)
-    .sin_port = htons(9001),
+    .sin_port = htons(PORT),
   };
 
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -64,7 +131,7 @@ int server_fd;
       exit(EXIT_FAILURE);
     }
 
-    int obj = open("object", O_CREAT);
+    int obj = open("object", O_CREAT | O_WRONLY, 0755);
     if (obj == -1) {
       perror("could not create object");
       exit(EXIT_FAILURE);
@@ -79,7 +146,7 @@ int server_fd;
         exit(EXIT_FAILURE);
       }
 
-      if (write(accept_fd, buffer, read_ret - 1) == -1) {
+      if (write(obj, buffer, read_ret) == -1) {
         perror("problem writing");
         exit(EXIT_FAILURE);
       }
